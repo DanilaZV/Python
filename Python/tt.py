551900d82c2cc76af538b2bc0831a48e0f88af7f
Danila <danya_zimin_04@mail.ru>
2022-09-21 19:58:28 +0300

    Please enter the commit message for your changes. Lines starting

--- Python/tt.py
+++ Python/tt.py
@@ -9,3 +9,4 @@ t.write("NIKITA GEY",  False, align="left", font=('Arial', 50, 'normal'))
 t.write("NIKITA GEY",  False, align="center", font=('Arial', 60, 'normal'))
 t.write("NIKITA GEY",  True, align="right", font=('Arial', 50, 'normal'))
 turtle.exitonclick()
+#fsdfsdfsfsf
