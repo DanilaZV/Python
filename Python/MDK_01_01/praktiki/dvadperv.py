b68d8fec03d1cefb67ef093453ded194871cfb1c
Vor360 <ert@narfu.ru>
2022-09-22 21:27:11 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ Python/MDK_01_01/praktiki/dvadperv.py
@@ -0,0 +1,10 @@
+a = ['катя', 'маша', 'таня', 'саша']
+
+b = list(map(lambda x: x.title(), a))
+print('Исходный список')
+
+print(a)
+
+print('Конечный список')
+
+print(b)
---
+++ Python/MDK_01_01/praktiki/pitoe.py
@@ -0,0 +1,6 @@
+
+n = int(input())
+nn = n % 10
+n //= 10
+n = nn * 100 + n
+print(n) 
