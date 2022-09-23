75394a8789f0c95a742b91ab95994334b1200726
Vor360 <ert@narfu.ru>
2022-09-23 12:19:07 +0300

    Please enter the commit message for your changes. Lines starting

--- Python/MDK_01_01/praktiki/chetrnadcatoe.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-n = int(input())
-a = 11
- 
-x = [i for i in range(n)]
-#print(x)
- 
-y = [i for i in x if i % 11 == 0]
-print(y)
--- Python/MDK_01_01/praktiki/gssdsd.py
+++
@@ -1,7 +0,0 @@
-n, a = int(input()), int(input())
-
-for _ in range(n-1):
-    x = [i for i in range(n)]
-    #lst.append(a + x)
-    a = x
-print(x)
